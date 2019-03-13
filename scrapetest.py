b9aff39748088d68c6f785371a996162794df0da
Csriteja <csriteja.1110@gmail.com>
2019-03-14 01:27:04 +0530

    Update scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -1,3 +1,5 @@
 from urllib.request import urlopen
-html = urlopen("https://pythonscraping.com/pages/page1.html")
-print(html.read())
+from bs4 import BeautifulSoup
+html = urlopen("https://sofascore.com")
+bsObj = BeautifulSoup(html.read())
+print(bsObj.h1)
