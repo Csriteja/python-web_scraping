b3f95404f8235377aafeeddd2f693dbc610a62a5
Csriteja <csriteja.1110@gmail.com>
2019-03-14 01:38:03 +0530

    Updated scrapetest.py to catch exceptions
    Used BeautifulSoup to extract the title from a web page's html. Also added checks for the various exceptions that might occur.

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,19 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("https://sofascore.com")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+title = getTitle("https://sofascore.com")
+if title == None:
+    print("Title Not Found")
+else:
+    print(title)
